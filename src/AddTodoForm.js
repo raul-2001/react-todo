ab3cde26322466c73c757296a3d6180226340c95
raul-2001 <rlqv2001@gmail.com>
2023-10-17 22:10:12 -0400

    lesson.1.3 first commit, useState and properties of JSX elements clallback handlers

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 // Funtion decleared
-let AddTodoForm = () => (
-    <form>
-        <label htmlFor="todoTitle"></label>
-        <input id="todoTitle" type="text"></input>
-        <button type="button">Add</button>
+let AddTodoForm = (props) => { 
+
+    const handleAddTodo = (event) =>{
+        event.preventDefault();
+        const todoTitle = event.target.title.value;
+        console.log(todoTitle);
+        event.target.reset();
+        props.onAddTodo(todoTitle);
+    }
+
+    return(
+    <form onSubmit={handleAddTodo}>
+        <label htmlFor="todoTitle">Title</label>
+        <input id="todoTitle" type="text" name="title" ></input>
+        <button type="button" >Add</button>
     </form>
 );
+}
 
 
 export default AddTodoForm;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,38 +1,41 @@
 import React from "react";
+import TodoListItem from "./TodoListItem";
 
 // list decleared
 const todoList = [
-    {
-      id:1,
-      title:"Start the react Flamingo class."
-    },
-    {
-      id: 2,
-      title: "Complete assignment."
-    },
-    {
-      id: 3,
-      title: "Submit your second lesson assignment."
-    },
-    {
-    id: 4,
-    title: "Submit all your lessons of Flamingo class."
-    }
-  ];
+  {
+    id:1,
+    title:"Start the react Flamingo class."
+  },
+  {
+    id: 2,
+    title: "Complete assignment."
+  },
+  {
+    id: 3,
+    title: "Submit your second lesson assignment."
+  },
+  {
+  id: 4,
+  title: "Submit all your lessons of Flamingo class."
+  }
+];
+
 
 // Funtion decleared
 let TodoList = () => (
-        <div>
-        {/* Show list items here */}
-        <ul>
-            {todoList.map(function(item) {
-            return (
-            <li key={item.id}>{item.title}</li>
-                );
-                }
-            )}
-        </ul>
-        </div>
+  <div>
+    {/* Show list items here */}
+    <ul>
+        {todoList.map(function(item) {
+        return (
+          <TodoListItem key={item.id} title={item.title}/>
+            );
+            }
+        )}
+  </ul>
+  </div>
 );
+
 
 export default TodoList;
