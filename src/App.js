bc2e4cb770f3e6b7492b246365ce54bc1dae8112
raul-2001 <rlqv2001@gmail.com>
2024-02-06 23:51:30 -0500

    TodoContainer and PropsType

--- src/App.js
+++ src/App.js
@@ -1,213 +1,52 @@
 import React from 'react';
-import TodoList from './components/TodoList';
-import AddTodoForm from './components/AddTodoForm';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styles from  './components/TodoListItem.module.css';
+import HomePage from './HomePage';
+import { useState } from 'react';
+import TodoContainer from './components/TodoContainer.js'
+import { Link } from 'react-router-dom';
 
 
+const TableChooser = () => {
+  const [tableName, setTableName] = useState(process.env.REACT_APP_TABLE_NAME);
+  return (
+    <div>
+      <select
+        id="selectField"
+        value={tableName}
+        onChange={(event) => setTableName(event.target.value)}
+      >
+        <option value={process.env.REACT_APP_TABLE_NAME}>Todo List</option>
+    
+      </select>
+      <TodoContainer tableName={tableName} />
+    </div>
+  );
+};
 
 
 // View part of react
 const App = () => {
 
-
-  const [todoList, setTodoList] = React.useState(
-    []
-  );
-
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  const [isError, setIsError] = React.useState(false);
-
-  // GET 
-  const fetchData = async () => {
-
-    setIsLoading(true);
-    
-    const options = {
-      method: "GET",
-      headers: {
-        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`
-      }
-    }
-    
-    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/`;
-
-    try {
-      const response = await fetch(url, options);
-      
-      if (!response.ok) {
-        const message = `Error: ${response.status}`;
-        throw new Error(message);
-      }
-
-      const data = await response.json();
-
-      const todos = data.records.map((todo) => {
-        const newTodos = {
-          id: todo.id,
-          title: todo.fields.title,
-        }
-
-        return newTodos
-
-      });
-      
-      setTodoList(todos)
-
-    } catch (error) {
-      setIsError(error);
-      console.log(error.message)
-    } finally {
-      setIsLoading(false);
-    }
-    
-  };
-
-
-  React.useEffect(() => {
-    fetchData();
-  }, [])
-
-  const addTodo = (records) => {
-    
-    try {
-      const response = postTodo(records[0].fields);
-
-      if (response) {
-        // setTodoList([...todoList, response]);
-        fetchData();
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-
-  // Creates new todo into Airtable table: 'POST'
-  const postTodo = async (todo) => {
-
-    try {
-
-      if (todo.length === 0 ) {
-        return 
-      };
-
-      const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
-        },
-        body: JSON.stringify({fields: todo}),
-      }
-  
-      const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/`;
-
-      const response = await fetch(
-        url, options
-      );
-
-      if (!response.ok) {
-        const message = `Error has accured: ${response.status}`;
-        throw new Error(message);
-      }
-
-      const responseData = await response.json();
-
-      return responseData;
-
-    } catch (error) {
-      setIsError(error);
-      console.log(error.message);
-      return null;
-    }
-  }
-
-  // Deletes todo from Airtable table: 'DELETE'
-  const deleteTodo = async (todo) => {
-
-    try {
-
-      if (!todo) {
-        return 
-      };
-
-      const options = {
-        method: 'DELETE',
-        headers: {
-          'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
-        },
-      }
-  
-      const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/${todo}`;
-
-      const response = await fetch(
-        url, options
-      );
-
-      if (!response.ok) {
-        const message = `Error has accured: ${response.status}`;
-        throw new Error(message);
-      }
-
-      const deletedData = await response.json();
-
-      return deletedData;
-      
-
-    } catch (error) {
-      setIsError(error);
-      console.log(error.message);
-      return null;
-    }
-  }
-  
-  
-  const removeTodo = (id) => {
-    const updatedList = todoList.filter(
-      removedItem => removedItem.id !== id
-    );
-    
-    // id for DELETE method
-    const deletedList = todoList.filter(
-      remitem => remitem.id === id
-    );
-
-    const delId = deletedList[0].id;
-    deleteTodo(delId);
-
-    setTodoList(updatedList);
-  }
-
-  // useEffect for storing data to local storage
-  React.useEffect(() => {
-    
-    if (isLoading) {
-      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-    }
-
-  }, [todoList, isLoading])
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-        <div className={styles.container}>
-          <h1 className={styles.headlinePrimary}>Todo List</h1>
-          <AddTodoForm onAddTodo={addTodo} />
-          <hr />
-          {isError && <p>Something went wrong ...</p>}
-          {isLoading ? <div>Loading ...</div> :
-            (<TodoList todoList={todoList} onRemoveTodo={removeTodo} />)
+        <Route path="/" element={<HomePage />} />
+        <Route path="/TodoList" element={<TableChooser />} />
+        <Route
+          path="/NewTodoList"
+          element={
+            <>
+              <button>
+                <Link to="/" style={{ color: "black", textDecoration: "none" }}>
+                  Back
+                </Link>
+              </button>
+              <h1>New Todo List</h1>
+            </>
           }
-        </div> 
-        } />
-        <Route path="/new" element={<>
-          <h1>New Todo List</h1>
-          </>}
         />
-      </Routes>  
+      </Routes>
     </BrowserRouter>
   );
 }
